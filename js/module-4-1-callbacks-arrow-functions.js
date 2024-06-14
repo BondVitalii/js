9b84ab0b4d8209f8a5320b36379012812f285f8c
BondVitalii <122458459+BondVitalii@users.noreply.github.com>
2024-06-14 21:35:44 +0300

    fix

---
+++ js/module-4-1-callbacks-arrow-functions.js
@@ -0,0 +1,27 @@
+// // Колбек-функція
+// function greet(name) {
+//   console.log(`Ласкаво просимо ${name}.`);
+// }
+
+// // Функція вищого порядку
+// function registerGuest(name, callback) {
+//   console.log(`Реєструємо гостя ${name}.`);
+//   callback(name);
+// }
+
+// registerGuest('Манго', greet);
+
+function registerGuest(name, callback) {
+  console.log(`Реєструємо гостя ${name}.`);
+  callback(name);
+}
+
+// Передаємо інлайн функцію greet у якості колбека
+registerGuest('Манго', function greet(name) {
+  console.log(`Ласкаво просимо ${name}.`);
+});
+
+// Передаємо інлайн функцію notify у якості колбека
+registerGuest('Полі', function notify(name) {
+  console.log(`Шановний(а) ${name}, ваш номер буде готовий за 30 хвилин.`);
+});
