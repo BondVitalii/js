ac77523ecca3768c98881f43a1f71894103212c8
BondVitalii <122458459+BondVitalii@users.noreply.github.com>
2024-07-02 23:22:25 +0300

    fix

--- js/autocheck2.js
+++ js/autocheck2.js
@@ -1,3 +1,83 @@
+// Этот массив объектов мы будем передавать в параметр users при вызове функции из задания.
+const users = [
+  {
+    name: 'Moore Hensley',
+    email: 'moorehensley@indexia.com',
+    eyeColor: 'blue',
+    friends: ['Sharron Pace'],
+    isActive: false,
+    balance: 2811,
+    skills: ['ipsum', 'lorem'],
+    gender: 'male',
+    age: 37,
+  },
+  {
+    name: 'Sharlene Bush',
+    email: 'sharlenebush@tubesys.com',
+    eyeColor: 'blue',
+    friends: ['Briana Decker', 'Sharron Pace'],
+    isActive: true,
+    balance: 3821,
+    skills: ['tempor', 'mollit', 'commodo', 'veniam', 'laborum'],
+    gender: 'female',
+    age: 34,
+  },
+  {
+    name: 'Ross Vazquez',
+    email: 'rossvazquez@xinware.com',
+    eyeColor: 'green',
+    friends: ['Marilyn Mcintosh', 'Padilla Garrison', 'Naomi Buckner'],
+    isActive: false,
+    balance: 3793,
+    skills: ['nulla', 'anim', 'proident', 'ipsum', 'elit'],
+    gender: 'male',
+    age: 24,
+  },
+  {
+    name: 'Elma Head',
+    email: 'elmahead@omatom.com',
+    eyeColor: 'green',
+    friends: ['Goldie Gentry', 'Aisha Tran'],
+    isActive: true,
+    balance: 2278,
+    skills: ['adipisicing', 'irure', 'velit'],
+    gender: 'female',
+    age: 21,
+  },
+  {
+    name: 'Carey Barr',
+    email: 'careybarr@nurali.com',
+    eyeColor: 'blue',
+    friends: ['Jordan Sampson', 'Eddie Strong'],
+    isActive: true,
+    balance: 3951,
+    skills: ['ex', 'culpa', 'nostrud'],
+    gender: 'male',
+    age: 27,
+  },
+  {
+    name: 'Blackburn Dotson',
+    email: 'blackburndotson@furnigeer.com',
+    eyeColor: 'brown',
+    friends: ['Jacklyn Lucas', 'Linda Chapman'],
+    isActive: false,
+    balance: 1498,
+    skills: ['non', 'amet', 'ipsum'],
+    gender: 'male',
+    age: 38,
+  },
+  {
+    name: 'Sheree Anthony',
+    email: 'shereeanthony@kog.com',
+    eyeColor: 'brown',
+    friends: ['Goldie Gentry', 'Briana Decker'],
+    isActive: true,
+    balance: 2764,
+    skills: ['lorem', 'veniam', 'culpa'],
+    gender: 'female',
+    age: 39,
+  },
+];
 /** Задача-1: 
 |============================
 
@@ -6,7 +86,7 @@
 
 
 // ------------------------------------------------------------
-
+console.log();
 // ___________________________________________________________________________________________
 // ======= Теория к задаче- ==================================================================
 
@@ -24,7 +104,7 @@
 * Вызов функции со случайными, но валидными аргументами, возвращает правильное значение
 
 Этот массив объектов мы будем передавать в параметр users при вызове функции из задания.
-[
+const users = [
   {
     name: "Moore Hensley",
     email: "moorehensley@indexia.com",
@@ -106,15 +186,380 @@
 
 const getUserNames = users => {
     
-    };
+};
 
 // ================== Решение ==================
 
 const getUserNames = users => {
   return users.map(user => user.name);
-  };
+};
 
-  console.log(); // ["Moore Hensley", "Sharlene Bush", "Ross Vazquez", "Elma Head", "Carey Barr", "Blackburn Dotson", "Sheree Anthony"]
+console.log(getUserNames(users)); // ["Moore Hensley", "Sharlene Bush", "Ross Vazquez", "Elma Head", "Carey Barr", "Blackburn Dotson", "Sheree Anthony"]
 |============================
 */
 // ===========================================================================================
+/** Задача-18: Метод map()
+|============================
+Дополни функцию getUserEmails(users) так, чтобы она возвращала массив почтовых адресов пользователей (свойство email) из массива объектов в параметре users.
+
+Объявлена переменная getUserNames
+Переменной getUserNames присвоена стрелочная функция с параметром (users)
+Для перебора параметра users используется метод map()
+Вызов функции с указанным массивом пользователей возвращает массив ["moorehensley@indexia.com", "sharlenebush@tubesys.com", "rossvazquez@xinware.com", "elmahead@omatom.com", "careybarr@nurali.com", "blackburndotson@furnigeer.com", "shereeanthony@kog.com"]
+Вызов функции со случайными, но валидными аргументами, возвращает правильное значение
+
+const getUserEmails = users => {
+  
+};
+
+// ================== Решение ==================
+const getUserEmails = users => {
+  return users.map(user => user.email);
+};
+// ------------------------------------------------------------
+console.log(getUserEmails(users));               // ["moorehensley@indexia.com", "sharlenebush@tubesys.com", "rossvazquez@xinware.com", "elmahead@omatom.com", "careybarr@nurali.com", "blackburndotson@furnigeer.com", "shereeanthony@kog.com"]
+|============================
+*/
+// ===========================================================================================
+/** Задача-19: Метод filter()
+|============================
+Дополни код так, чтобы в переменной evenNumbers получился массив чётных чисел из массива numbers, а в переменной oddNumbers массив нечётных. Обязательно используй метод filter().
+
+Объявлена переменная numbers
+Значение переменной numbers это массив [17, 24, 82, 61, 36, 18, 47, 52, 73]
+Объявлена переменная evenNumbers
+Значение переменной evenNumbers это массив [24, 82, 36, 18, 52]
+Объявлена переменная oddNumbers
+Значение переменной oddNumbers это массив [17, 61, 47, 73]
+Для перебора массива numbers использован метод filter()
+
+const numbers = [17, 24, 82, 61, 36, 18, 47, 52, 73];
+
+const evenNumbers = numbers;
+const oddNumbers = numbers;
+
+// ================== Решение ==================
+const numbers = [17, 24, 82, 61, 36, 18, 47, 52, 73];
+
+const evenNumbers = numbers.filter(number => number % 2 === 0);
+const oddNumbers = numbers.filter(number => number % 2 === 1);;
+
+// ------------------------------------------------------------
+console.log(evenNumbers);              // [24, 82, 36, 18, 52]
+console.log(oddNumbers);               // [17, 61, 47, 73]
+
+// ___________________________________________________________________________________________
+// ======= Теория к задаче-19 ==================================================================
+
+Метод filter(callback) используется для единственной операции - фильтрации массива, то есть когда необходимо выбрать более одного элемента из коллекции по какому-то критерию.
+
+массив.filter((element, index, array) => {
+  // Тело коллбек-функции
+});
+Не изменяет оригинальный массив.
+Поэлементно перебирает оригинальный массив.
+Возвращает новый массив.
+Добавляет в возвращаемый массив элементы которые удовлетворяют условию коллбек-функции.
+Если коллбек вернул true элемент добавляется в возвращаемый массив.
+Если коллбек вернул false элемент не добавляется в возвращаемый массив.
+Если ни один элемент не удовлетворил условию, возвращает пустой массив.
+const values = [51, -3, 27, 21, -68, 42, -37];
+
+const positiveValues = values.filter(value => value >= 0);
+console.log(positiveValues); // [51, 27, 21, 42]
+
+const negativeValues = values.filter(value => value < 0);
+console.log(negativeValues); // [-3, -68, -37]
+
+const bigValues = values.filter(value => value > 1000);
+console.log(bigValues); // []
+
+// Оригинальный массив не изменился
+console.log(values); // [51, -3, 27, 21, -68, 42, -37]
+То есть метод filter вызывает коллбек-функцию для каждого элемента исходного массива и если результат её выполнения true, текущий элемент добавляет в новый массив.
+
+|============================
+*/
+// ===========================================================================================
+/** Задача-20: Метод filter() + indexOf() Уникальные элементы
+|============================
+Дополни код так, чтобы в переменной allGenres был массив всех жанров книг (свойство genres) из массива books, а в переменной uniqueGenres массив уникальных жанров - без повторений.
+
+Объявлена переменная books
+Значение переменной books это массив объектов
+Объявлена переменная allGenres
+Значение переменной allGenres это массив ["adventure", "history", "fiction", "mysticism", "horror", "mysticism", "adventure"]
+Объявлена переменная uniqueGenres
+Значение переменной uniqueGenres это массив ["adventure", "history", "fiction", "mysticism", "horror"]
+Для вычисления значения переменной allGenders использован метод flatMap()
+Для вычисления значения переменной uniqueGenres использован метод filter()
+
+const books = [
+  {
+    title: "The Last Kingdom",
+    author: "Bernard Cornwell",
+    genres: ["adventure", "history"],
+  },
+  {
+    title: "Beside Still Waters",
+    author: "Robert Sheckley",
+    genres: ["fiction", "mysticism"],
+  },
+  {
+    title: "Redder Than Blood",
+    author: "Tanith Lee",
+    genres: ["horror", "mysticism", "adventure"],
+  },
+];
+
+const allGenres = books;
+const uniqueGenres = allGenres;
+
+// ================== Решение ==================
+
+const books = [
+  {
+    title: "The Last Kingdom",
+    author: "Bernard Cornwell",
+    genres: ["adventure", "history"],
+  },
+  {
+    title: "Beside Still Waters",
+    author: "Robert Sheckley",
+    genres: ["fiction", "mysticism"],
+  },
+  {
+    title: "Redder Than Blood",
+    author: "Tanith Lee",
+    genres: ["horror", "mysticism", "adventure"],
+  },
+];
+
+const allGenres = books.flatMap(book => book.genres);
+const uniqueGenres = allGenres.filter((genre, index, array) => array.indexOf(genre) === index);
+
+
+// ------------------------------------------------------------
+console.log(allGenres); // ["adventure", "history", "fiction", "mysticism", "horror", "mysticism", "adventure"]
+console.log(uniqueGenres);  // ["adventure", "history", "fiction", "mysticism", "horror"]
+// ___________________________________________________________________________________________
+// ======= Теория к задаче-20 ==================================================================
+
+Используя метод filter() можно выполнить фильтрацию массива так, что в нём останутся только уникальные элементы. Этот приём работает только с массивом примитивных значений - не объектов.
+
+Вернёмся к группе студентов и массиву всех посещаемых предметов, которые мы получили методом flatMap().
+
+const students = [
+  { name: "Mango", courses: ["mathematics", "physics"] },
+  { name: "Poly", courses: ["science", "mathematics"] },
+  { name: "Kiwi", courses: ["physics", "biology"] },
+];
+
+const allCourses = students.flatMap(student => student.courses);
+// ["mathematics", "physics", "science", "mathematics", "physics", "biology"];
+В переменной allCourses хранится массив всех посещаемых предметов, которые могут повторяться. Задача заключается в том, чтобы сделать новый массив, в котором будут только уникальные предметы, то есть без повторений.
+
+const uniqueCourses = allCourses.filter(
+  (course, index, array) => array.indexOf(course) === index
+);
+Используя array.indexOf(course) выполняем поиск первого совпадения текущего элемента course и получаем его индекс в оригинальном массиве всех курсов. В параметре index хранится индекс текущего элемента course при переборе массива методом filter.
+
+Если результат indexOf() и значение index равны - это уникальный элемент, потому что это первый раз когда такое значение встречается в массиве и на текущей итерации фильтр обрабатывает именно его.
+
+# Массив всех курсов
+["mathematics", "physics", "science", "mathematics", "physics", "biology"];
+Для элемента "mathematics" под индексом 0:
+
+indexOf() вернёт 0, потому что ищет первое совпадение.
+Значение параметра index будет 0.
+Они равны, значит это уникальный элемент.
+Для элемента "mathematics" под индексом 3:
+
+indexOf() вернёт 0, потому что ищет первое совпадение.
+Значение параметра index будет 3.
+Они не равны, значит это повторяющийся - не уникальный элемент.
+
+|============================
+*/
+// ===========================================================================================
+/** Задача-21: 
+|============================
+Используя метод filter() дополни код так, чтобы:
+
+В переменной topRatedBooks получился массив книг рейтинг которых (свойство rating) больше либо равно значению переменной MIN_RATING.
+В переменной booksByAuthor получился массив книг написанных автором с именем (свойство author) которое совпадает со значением в переменной AUTHOR.
+Объявлена переменная books
+Значение переменной books это массив объектов
+Объявлена переменная MIN_RATING
+Значение переменной MIN_RATING это число 8
+Объявлена переменная AUTHOR
+Значение переменной AUTHOR это строка "Bernard Cornwell"
+Объявлена переменная topRatedBooks
+Значение переменной topRatedBooks это массив книг с рейтингов выше 8
+Объявлена переменная booksByAuthor
+Значение переменной booksByAuthor это массив книг автор которых "Bernard Cornwell"
+Для перебора массива books использован метод filter()
+
+const books = [
+  {
+    title: "The Last Kingdom",
+    author: "Bernard Cornwell",
+    rating: 8.38,
+  },
+  {
+    title: "Beside Still Waters",
+    author: "Robert Sheckley",
+    rating: 8.51,
+  },
+  {
+    title: "The Dream of a Ridiculous Man",
+    author: "Fyodor Dostoevsky",
+    rating: 7.75,
+  },
+  { title: "Redder Than Blood", author: "Tanith Lee", rating: 7.94 },
+  { title: "Enemy of God", author: "Bernard Cornwell", rating: 8.67 },
+];
+
+const MIN_RATING = 8;
+const AUTHOR = "Bernard Cornwell";
+
+const topRatedBooks = books;
+const booksByAuthor = books;
+
+// ================== Решение ==================
+
+const MIN_RATING = 8;
+const AUTHOR = "Bernard Cornwell";
+
+const topRatedBooks = books.filter(book => book.rating >= MIN_RATING);
+const booksByAuthor = books.filter(book => book.author === AUTHOR);
+
+// ------------------------------------------------------------
+console.log(topRatedBooks);
+console.log(booksByAuthor);
+// ___________________________________________________________________________________________
+// ======= Теория к задаче-21 ==================================================================
+При работе с массивом объектов выполняется фильтрация по значению какого-то свойства. В результате получается новый массив отфильтрованных объектов.
+
+Например, есть массив студентов с баллами за тест. Необходимо отфильтровать лучших (балл выше 80), худших (балл ниже 50) и средних студентов (балл от 50 до 80).
+
+const LOW_SCORE = 50;
+const HIGH_SCORE = 80;
+const students = [
+  { name: "Mango", score: 83 },
+  { name: "Poly", score: 59 },
+  { name: "Ajax", score: 37 },
+  { name: "Kiwi", score: 94 },
+  { name: "Houston", score: 64 },
+];
+
+const best = students.filter(student => student.score >= HIGH_SCORE);
+console.log(best); // Массив объектов с именами Mango и Kiwi
+
+const worst = students.filter(student => student.score < LOW_SCORE);
+console.log(worst); // Массив с одним объектом Ajax
+
+// В коллбек-функции удобно деструктуризировать свойства объекта
+const average = students.filter(
+  ({ score }) => score >= LOW_SCORE && score < HIGH_SCORE
+);
+console.log(average); // Массив объектов с именами Poly и Houston
+
+|============================
+*/
+// ===========================================================================================
+/** Задача-22: 
+|============================
+Дополни функцию getUsersWithEyeColor(users, color) так, чтобы она возвращала массив пользователей у которых цвет глаз (свойство eyeColor) совпадает со значением параметра color.
+
+Объявлена переменная getUsersWithEyeColor
+Переменной getUsersWithEyeColor присвоена стрелочная функция с параметрами (users, color)
+Для перебора параметра users используется метод filter()
+Если значение параметра color это "blue", функция возвращает массив объектов пользователей с именами Moore Hensley, Sharlene Bush и Carey Barr
+Если значение параметра color это "green", функция возвращает массив объектов пользователей с именами Ross Vazquez и Elma Head
+Если значение параметра color это "brown", функция возвращает массив объектов пользователей с именами Blackburn Dotson и Sheree Anthony
+Если значение параметра color это любая другая строка, функция возвращает пустой массив
+Вызов функции со случайными, но валидными аргументами, возвращает правильное значение
+
+
+Этот массив объектов мы будем передавать в параметр users при вызове функции из задания.
+[
+  {
+    name: "Moore Hensley",
+    email: "moorehensley@indexia.com",
+    eyeColor: "blue",
+    friends: ["Sharron Pace"],
+    isActive: false,
+    balance: 2811,
+    gender: "male"
+  },
+  {
+    name: "Sharlene Bush",
+    email: "sharlenebush@tubesys.com",
+    eyeColor: "blue",
+    friends: ["Briana Decker", "Sharron Pace"],
+    isActive: true,
+    balance: 3821,
+    gender: "female"
+  },
+  {
+    name: "Ross Vazquez",
+    email: "rossvazquez@xinware.com",
+    eyeColor: "green",
+    friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
+    isActive: false,
+    balance: 3793,
+    gender: "male"
+  },
+  {
+    name: "Elma Head",
+    email: "elmahead@omatom.com",
+    eyeColor: "green",
+    friends: ["Goldie Gentry", "Aisha Tran"],
+    isActive: true,
+    balance: 2278,
+    gender: "female"
+  },
+  {
+    name: "Carey Barr",
+    email: "careybarr@nurali.com",
+    eyeColor: "blue",
+    friends: ["Jordan Sampson", "Eddie Strong"],
+    isActive: true,
+    balance: 3951,
+    gender: "male"
+  },
+  {
+    name: "Blackburn Dotson",
+    email: "blackburndotson@furnigeer.com",
+    eyeColor: "brown",
+    friends: ["Jacklyn Lucas", "Linda Chapman"],
+    isActive: false,
+    balance: 1498,
+    gender: "male"
+  },
+  {
+    name: "Sheree Anthony",
+    email: "shereeanthony@kog.com",
+    eyeColor: "brown",
+    friends: ["Goldie Gentry", "Briana Decker"],
+    isActive: true,
+    balance: 2764,
+    gender: "female"
+  }
+]
+
+const getUsersWithEyeColor = (users, color) => {
+ 
+};
+
+// ================== Решение ==================
+
+const getUsersWithEyeColor = (users, color) => {
+  return users.filter(user => user.eyeColor === color);
+};
+
+// ------------------------------------------------------------
+console.log();
+|============================
+*/
