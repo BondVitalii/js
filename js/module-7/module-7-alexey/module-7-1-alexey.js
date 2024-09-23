1f4bc3e3e62769adff276fa1a05334512f9f4a9f
BondVitalii <122458459+BondVitalii@users.noreply.github.com>
2024-09-23 13:03:39 +0300

    fix

--- js/module-7/module-7-alexey/module-7-1-alexey.js
+++ js/module-7/module-7-alexey/module-7-1-alexey.js
@@ -43,7 +43,6 @@ for (const el of allElements) {
 Кольорова підказка має змінювати своє значення при перетягувані повзунка.
 В інпуті №2 мін і мах поріг встановлюєте самостійно.
 При завантаженні сторінки сприпт має автоматично розрахувати вартість на основі даних за замовчуванням які ви встановите в розмітці.
-
 // -----------------------------------------
 ** Мікрозадача 1: зміна кількості кілограм під час зміни повзунка
 1. отримати refs
@@ -56,7 +55,6 @@ for (const el of allElements) {
 2. заповнити поля обʼєкту значеннями з інпутів
 3. вивести результат розрахунку на екран
 // -----------------------------------------
-
 const refs = {
   form: document.getElementById('form'),
   amount: document.getElementById('amount'),
@@ -65,13 +63,10 @@ const refs = {
   quantity: document.getElementById('quantity'),
 };
 
-// Создаем объект с свойствами и методом. Для умножения кол-ва на стоимость.
-
 const data = {
   price: 0,
   quantity: 0,
 
-  // Метод высчитывающий финальную стоимость продукта
   calcTotalPrice() {
     return Number((this.price * this.quantity).toFixed(2));
   },
@@ -81,20 +76,69 @@ dataFill();
 displayTotal();
 updateAmount();
 
-refs.form.addEventListener('input', handleFormInput); // Вешаем слушателя на форму.
+refs.form.addEventListener('input', handleFormInput);
 
-// Деструктуризируем event.target в target
 function handleFormInput({ target }) {
+  if (target.name === 'quantity') updateAmount();
+
+  target.setAttribute('value', target.value);
+  dataFill();
+  displayTotal();
+}
+
+function dataFill() {
+  data.price = Number(refs.price.value);
+  data.quantity = Number(refs.quantity.value);
+}
+
+function displayTotal() {
+  const totalPrice = data.calcTotalPrice();
+
+  refs.total.textContent =
+    (Number.isInteger(totalPrice) ? `${totalPrice}.00` : totalPrice) + ' грн';
+}
+
+function updateAmount() {
+  refs.amount.textContent = quantity.value;
+}
+
+// ----------------------------------------------------------------------------
+// Ниже тот же самый код, но с разъяснением!
+// ----------------------------------------------------------------------------
+
+const refs = {
+  form: document.getElementById('form'),
+  amount: document.getElementById('amount'),
+  total: document.getElementById('total'),
+  price: document.getElementById('price'),
+  quantity: document.getElementById('quantity'),
+};
+
+const data = {   // Создаем объект с свойствами и методом. Для умножения кол-ва на стоимость.
+  price: 0,
+  quantity: 0,
+
+  calcTotalPrice() {                     // Метод высчитывающий финальную стоимость продукта.
+    return Number((this.price * this.quantity).toFixed(2));
+  },
+};
+
+dataFill();
+displayTotal();
+updateAmount();
+
+refs.form.addEventListener('input', handleFormInput);           // Вешаем слушателя на форму.
+
+function handleFormInput({ target }) {            // Деструктуризируем event.target в target.
   // console.log(target.name);
   if (target.name === 'quantity') updateAmount(); // Проверка, если клик пришел из инпута quantity,то...
 
   target.setAttribute('value', target.value);  // Изменение значения атрибута value в инпуте на текущее.
-  dataFill();                            // Вызов функции.
-  displayTotal();                        // Вызов функции.
+  dataFill();                                      // Вызов функции.
+  displayTotal();                                  // Вызов функции.
 }
 
 // ** Функция заполнения свойств объекта data из соответствующих полей инпутов.
-
 function dataFill() {
   data.price = Number(refs.price.value);     // Запись знач.поля(инпута price), в свойство price объекта data.
   data.quantity = Number(refs.quantity.value); // Запись знач.поля(инпута quantity), в свойство quantity объекта data.
@@ -103,21 +147,18 @@ function dataFill() {
 }
 
 // ** Функция показа результата умножения товара на стоимость.
-
 function displayTotal() {
   const totalPrice = data.calcTotalPrice();              // Константа сохраняющая финальную цену.
 
-  // Если целое число,то добавляем в конце два ноля, если чило не целое,то выводим его результат.
+  // Если число целое,то добавляем в конце два ноля, если не целое,то выводим его результат.
   refs.total.textContent =
     (Number.isInteger(totalPrice) ? `${totalPrice}.00` : totalPrice) + ' грн';
 }
 
 // ** Функция обновления показа кол-ва в span, соответствии с кол-вом в инпуте quantity.
-
 function updateAmount() {
   refs.amount.textContent = quantity.value;
 }
 |============================
 */
 // =============================================================
-// -------------------------------------------------------------------
